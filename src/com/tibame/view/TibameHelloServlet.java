d8e4fd6f851893f43d43f30353b0cae24f5e7434
YenKang <rcd1988715@gmail.com>
2021-01-18 18:46:38 +0800

    property injection like setter companyName

--- src/com/tibame/view/TibameHelloServlet.java
+++ src/com/tibame/view/TibameHelloServlet.java
@@ -16,16 +16,17 @@ import com.tibame.domain.TibameHello;
 @WebServlet("/TibameHelloServlet")
 public class TibameHelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml"); 
-
+	private ClassPathXmlApplicationContext factory = 
+			new ClassPathXmlApplicationContext("applicationContext.xml"); 
+	
 	public TibameHelloServlet() {
 		super();
 	}
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//ITibameHello hello = new TibameHello();
-		ITibameHello hello = factory.getBean("tibamehello", ITibameHello.class);
+		ITibameHello hello = factory.getBean("tibamehello2", ITibameHello.class);
 		String msg = hello.helloWorld("Tom");
-		response.getWriter().println(msg);
+		response.getWriter().println(msg); // UI display
 		
 	}
 
