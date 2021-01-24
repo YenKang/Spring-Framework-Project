ec257b3c1a97600f357c5a0a994397c0d574f5c7
YenKang <rcd1988715@gmail.com>
2021-01-24 22:46:06 +0800

    still working on Lab1 (MySQL)

---
+++ src/com/tibame/domain/Lab1MySQL.java
@@ -0,0 +1,10 @@
+package com.tibame.domain;
+
+public class Lab1MySQL {
+	private String url;
+	private String databasename;
+	private String driverClassName;
+	private String username;
+	private String password;
+	
+}
---
+++ src/com/tibame/view/Lab1Servlet.java
@@ -0,0 +1,24 @@
+package com.tibame.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class Lab1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	public Lab1Servlet() {
+		super();
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
